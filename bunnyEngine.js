cac4bfb25d4a97e802ae29ac3560a3899b6925b8
Leo Bottaro <leobottaro@gmail.com>
2018-02-23 18:01:25 -0300

    start of bunny Engine

---
+++ bunnyEngine.js
@@ -0,0 +1,23 @@
+let game = new Main()
+game.start()
+
+class Main {
+    constructor() {
+
+    }
+    start() {
+        run()
+    }
+    stop() {
+
+    }
+    run() {
+
+    }
+    render() {
+
+    }
+    cleanUp() {
+
+    }
+}
