4dd27dabb2e3a9c041abf061212d7666d5c25c95
leocb <leobottaro@gmail.com>
2018-05-22 17:34:44 -0300

    Begin modifications

--- product-viz/main.js
+++ product-viz/main.js
@@ -2,19 +2,30 @@
 let scene = new THREE.Scene()
 let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 
-let renderer = new THREE.WebGLRenderer()
+let renderer = new THREE.WebGLRenderer({
+  antialias: true
+})
+renderer.setClearColor('#FFFFFF')
 renderer.setSize(window.innerWidth, window.innerHeight, false)
 document.body.appendChild(renderer.domElement)
 
+// Light?
+//let light = new THREE.LightShadow()
+
 // Creates a cube
-let geometry = new THREE.BoxGeometry(1, 1, 1)
-let material = new THREE.MeshBasicMaterial({
+let cubeGeometry = new THREE.BoxGeometry(3, 3, 3)
+let cubeMaterial = new THREE.MeshBasicMaterial({
   color: 0x00ff00
 })
 
-let cube = new THREE.Mesh(geometry, material)
+let cube = new THREE.Mesh(cubeGeometry, cubeMaterial)
 scene.add(cube)
 camera.position.z = 5
+
+// Loads model
+// let LoadingManager = new THREE.LoadingManager()
+// let JSONLoader = new THREE.JSONLoader(loaderManager)
+
 
 
 // Render loop
